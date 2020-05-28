2ceade2eca8c2aea1bc37f655fd1903155f55563
Hugo Peres <hugo-pdf@hotmail.com>
2020-05-28 16:16:11 -0300

    add cors at app

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from routes import router, api_router
 
 system = dict() # uptime, start time
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app)
 app.register_blueprint(router)
 app.register_blueprint(api_router)
 
