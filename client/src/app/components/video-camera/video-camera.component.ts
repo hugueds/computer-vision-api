8f3372fed42fc1e708426a34c32fcbfb0163af3a
Hugo Peres <hugo-pdf@hotmail.com>
2020-06-02 15:11:24 -0300

    camera improvements

--- client/src/app/components/video-camera/video-camera.component.ts
+++ client/src/app/components/video-camera/video-camera.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-video-camera',
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoCameraComponent implements OnInit {
 
+  @ViewChild('video', { static: true }) public video: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  public ngAfterViewInit() {
+
+    const constraints = {
+      video: true,
+      width: {
+        min: 640,
+        max: 640
+      },
+      height: {
+        min: 480,
+        max: 480
+      },
+      advanced: [{
+        facingMode: "environment"
+      }]
+    }
+
+
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: constraints }).then(stream => {
+        this.video.nativeElement.srcObject = stream
+        this.video.nativeElement.play();
+      });
+    }
+
   }
 
 }
